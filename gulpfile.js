d9edcf03e6c99647ae23ff8a0205e36ce799984d
andreiSteSte <andreistefanstefan1997@gmail.com>
2023-11-22 18:40:00 +0100

    Subir cambios

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,35 @@
 var gulp = require('gulp');
 var clean = require('gulp-clean');
 var sass = require('gulp-sass')(require('sass'));
-var rename = require('gulp-rename');
 var git = require('gulp-git');
 var cleanCSS = require('gulp-clean-css');
 
-//BORRAR LA CARPETA STYLES ANTES
+// BORRAR LA CARPETA STYLES ANTES
 gulp.task('borrar', function() {
-    return gulp.src('./styles/*', { read: false, allowEmpty: false })
-        .pipe(clean());
-})
+  return gulp.src('./styles/*', { read: false, allowEmpty: false })
+    .pipe(clean());
+});
 
-//GENERAR LAS HOJAS DE ESTILOS A PARTIR DE LOS FICHEROS .SCSS en Styles.
-
+// GENERAR LAS HOJAS DE ESTILOS A PARTIR DE LOS FICHEROS .SCSS en Styles.
 gulp.task('sass', function() {
-    return gulp.src('./scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('styles'));
+  return gulp.src('./scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('styles'));
 });
 
-
-//LIMPIAR CSS Y CONDENSARLO
-
+// LIMPIAR CSS Y CONDENSARLO
 gulp.task('minificarCSS', function() {
-    return gulp.src('styles/main.css')
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('styles'));
+  return gulp.src('styles/main.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('styles'));
 });
 
-//GIT COSAS
-
+// SUBIR EL PROYECTO A GITHUB
 gulp.task('subirGitHub', function() {
-    return gulp.src('.')
-        .pipe(git.add({args: '-A'}))
-        .pipe(git.commit('Subir cambios'))
-        .pipe(git.push('origin', 'master'));
+  return gulp.src('.')
+    .pipe(git.add())
+    .pipe(git.commit('Subir cambios'))
 });
 
-//Tarea:
+// Tarea:
 gulp.task('default', gulp.series('borrar', 'sass', 'minificarCSS', 'subirGitHub'));
